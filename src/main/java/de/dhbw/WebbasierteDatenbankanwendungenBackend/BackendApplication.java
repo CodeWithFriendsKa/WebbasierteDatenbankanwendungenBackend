80ad3c552f565d6dfd5e9748debee51ad5740b59
CodeWithFriendsKa <tamino.fischer@outlook.de>
2019-02-13 12:29:12 +0100

    "first commit"

---
+++ src/main/java/de/dhbw/WebbasierteDatenbankanwendungenBackend/BackendApplication.java
@@ -0,0 +1,14 @@
+package de.dhbw.WebbasierteDatenbankanwendungenBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
+
